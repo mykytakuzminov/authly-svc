f3c2d6774dc5880a50f18dd384c6a8aadf4d07c9
Mykyta Kuzminov <mykytakuzminov@gmail.com>
2026-06-28 15:00:53 +0200

    refactor: change user service name to auth service

--- services/auth_service/internal/domain/user.go
+++ services/auth_service/internal/domain/user.go
@@ -5,8 +5,6 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgconn"
 )
 
 type User struct {
@@ -23,17 +21,7 @@ type RegisterInput struct {
 	Password string
 }
 
-type DB interface {
-	Exec(ctx context.Context, sql string, args ...interface{}) (pgconn.CommandTag, error)
-	Query(ctx context.Context, sql string, args ...interface{}) pgx.Rows
-	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
-}
-
 type UserRepository interface {
 	Create(ctx context.Context, user *User) error
 	ExistsByEmail(ctx context.Context, email string) (bool, error)
-}
-
-type UserService interface {
-	Register(ctx context.Context, input *RegisterInput) (*TokenPair, error)
 }
