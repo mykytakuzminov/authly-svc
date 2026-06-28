8dbe28aae695c4237d646e309a4540510d8ab1f9
Mykyta Kuzminov <mykytakuzminov@gmail.com>
2026-06-28 16:25:27 +0200

    feat: implement jwt manager to generate and validate tokens

--- services/auth_service/internal/domain/errors.go
+++ services/auth_service/internal/domain/errors.go
@@ -7,7 +7,10 @@ import (
 )
 
 var (
-	ErrUserNotFound      = fmt.Errorf("user %w", errors.ErrNotFound)
-	ErrUserAlreadyExists = fmt.Errorf("user %w", errors.ErrAlreadyExists)
-	ErrTokenNotFound     = fmt.Errorf("token %w", errors.ErrNotFound)
+	ErrUserNotFound         = fmt.Errorf("user %w", errors.ErrNotFound)
+	ErrUserAlreadyExists    = fmt.Errorf("user %w", errors.ErrAlreadyExists)
+	ErrTokenNotFound        = fmt.Errorf("token %w", errors.ErrNotFound)
+	ErrInvalidSigningMethod = fmt.Errorf("%w signing method", errors.ErrInvalid)
+	ErrInvalidToken         = fmt.Errorf("%w token", errors.ErrInvalid)
+	ErrInvalidClaim         = fmt.Errorf("%w claim", errors.ErrInvalid)
 )
